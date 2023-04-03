80c67eccca99e75f965541aabbc32921a7913e69
Guillaume <guillaume.coiffier@inria.fr>
2023-04-03 15:04:02 +0200

    1.1.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as file_handle:
 
 setuptools.setup(
     name="mouette",
-    version='1.1.1',
+    version='1.1.2',
     author="GCoiffier",
     description="Mesh, Tools and Geometry Processing",
     long_description=long_description,
