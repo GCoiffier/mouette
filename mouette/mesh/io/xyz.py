3fa757ee4208d1c043c7cee02296e2bc951f29f0
GCoiffier <guillaume.coiffier.42@orange.fr>
2023-07-10 10:57:21 +0200

    read normal data from xyz

--- mouette/mesh/io/xyz.py
+++ mouette/mesh/io/xyz.py
@@ -1,6 +1,6 @@
 from ..mesh_data import RawMeshData
-from ...geometry import Vec
 from ..mesh import PointCloud
+import numpy as np
 
 def import_xyz(filepath : str) -> RawMeshData:
     """Imports a point cloud from a .xyz file on the disk
@@ -12,10 +12,16 @@ def import_xyz(filepath : str) -> RawMeshData:
         RawMeshData: parsed file
     """
     obj = RawMeshData()
+    nrmls = []
     with open(filepath, 'r') as f:
         for v in f.readlines():
-            v = [float(x) for x in v.strip().split()[:3]]
-            obj.vertices.append(v)
+            data = [float(x) for x in v.strip().split()]
+            obj.vertices.append(data[:3])
+            if len(data)>=6:
+                nrmls.append(data[3:6])
+    if len(nrmls)==len(obj.vertices):
+        normals = obj.vertices.create_attribute("normals", float, 3, dense=True)
+        normals._data = np.array(nrmls)
     return obj
 
 def export_xyz(mesh : PointCloud, filepath : str) -> None:
@@ -26,5 +32,11 @@ def export_xyz(mesh : PointCloud, filepath : str) -> None:
         filepath (str): path to the .xyz file
     """
     with open(filepath, 'w') as f:
-        for v in mesh.vertices:
-            f.write("{} {} {}\n".format(v[0], v[1], v[2]))
+        if mesh.vertices.has_attribute("normals"):
+            normals = mesh.vertices.get_attribute("normals")
+            for iv,v in enumerate(mesh.vertices):
+                nv = normals[iv]
+                f.write("{} {} {} {} {} {}\n".format(v[0], v[1], v[2], nv[0], nv[1], nv[2]))
+        else:
+            for v in mesh.vertices:
+                f.write("{} {} {}\n".format(v[0], v[1], v[2]))
