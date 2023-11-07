8b74d0cde2431d3d10679badcf708d9eacf20d9a
Guillaume <guillaume.coiffier@inria.fr>
2023-11-07 10:42:45 +0100

    discrete gradient operator

---
+++ examples/gradient.py
@@ -0,0 +1,64 @@
+import mouette as M
+from mouette import geometry as geom
+
+import scipy.sparse as sp
+import cmath
+import numpy as np
+import argparse
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_mesh", type=str, help="path to the input mesh")
+    parser.add_argument("-outp", "--outp", default="output.geogram_ascii")
+    parser.add_argument("-l", "--eigen", type=float, default=100., help="eigenvalue")
+    parser.add_argument("-signal", type=str, choices=["eigen", "z"])
+
+    args = parser.parse_args()
+
+    OUTPUT_FILE = args.outp
+    PREV = OUTPUT_FILE.split(".")
+    OUTPUT_GRAD = PREV[0] + "_grad." + PREV[1]
+    
+    print("Load mesh")
+    mesh = M.mesh.load(args.input_mesh)
+    connection = M.processing.SurfaceConnectionFaces(mesh)
+
+    if args.signal == "eigen":
+        print("Compute Laplacian")
+        lap = M.operators.laplacian(mesh)
+        A = M.operators.area_weight_matrix(mesh)
+
+        print("Compute eigenvector of laplacian near eigenvalue ", args.eigen)
+        eigvalue, signal = sp.linalg.eigsh(lap, M=A, k=1, sigma=args.eigen)
+        print("Found eigenvalue:", eigvalue[0])
+
+    elif args.signal =="z":
+        signal = np.zeros(len(mesh.vertices))
+        for iV,V in enumerate(mesh.vertices):
+            signal[iV] = V.z
+        signal = signal[:,np.newaxis]
+
+    print("Compute gradient")
+    grad = M.operators.gradient(mesh, connection)
+    signal_G = grad @ signal
+
+    print("Output results")
+    signal_attr = mesh.vertices.create_attribute("f", float, dense=True)
+    signal_attr._data = signal
+
+    grad_mesh = M.mesh.new_polyline()
+    L = M.attributes.mean_edge_length(mesh)/3
+    for id_face, face in enumerate(mesh.faces):
+        basis,Y = connection.base(id_face)
+        normal = geom.cross(basis,Y)
+        pA,pB,pC = (mesh.vertices[_v] for _v in face)
+        angle = cmath.phase(signal_G[id_face])
+        bary = (pA+pB+pC)/3 # reference point for display
+        direction = geom.rotate_around_axis(basis, normal, angle)
+        pt = bary + abs(signal_G[id_face])*L*direction
+        grad_mesh.vertices += [bary, pt]
+        grad_mesh.edges.append((2*id_face, 2*id_face+1))
+
+    M.mesh.save(mesh, OUTPUT_FILE)
+    M.mesh.save(grad_mesh, OUTPUT_GRAD)
+
---
+++ mouette/operators/gradient.py
@@ -0,0 +1,64 @@
+import numpy as np
+import scipy.sparse as sp
+import cmath
+
+from ..geometry import *
+from ..processing.connection import SurfaceConnectionFaces
+from ..mesh.datatypes import *
+from ..attributes.misc_faces import face_area
+
+@allowed_mesh_types(SurfaceMesh)
+def gradient(mesh : SurfaceMesh, conn : SurfaceConnectionFaces, as_complex: bool =True) -> sp.csc_matrix:
+    """
+    Computes the gradient operator, i.e. a [F| x |V| matrix G such that for any scalar function f defined over vertices of a surface mesh, Gf is its gradient inside faces.
+
+    Gf maps to each face of the mesh either a vector of R^2 or a complex number representing the gradient vector inside this face in local base.
+
+    Args:
+        mesh (SurfaceMesh): The input mesh
+        conn (SurfaceConnectionFaces): Connection objects specifying local bases of faces.
+        as_complex (bool, optional): whether the output is |F| complex values of 2|F| float values
+
+    Raises:
+        Exception: Fails if the mesh is not a triangulation
+    
+    Returns:
+        scipy.sparse.csc_matrix: Gradient operator
+    """
+    if not mesh.is_triangular():
+        raise Exception("Mesh is not a triangulation")
+    if conn is None : conn = SurfaceConnectionFaces(mesh)
+    area = face_area(mesh)
+
+    N = len(mesh.vertices)
+    M = len(mesh.faces)
+
+    if as_complex:
+        cols, rows, vals = np.zeros(3*M, dtype=int), np.zeros(3*M, dtype=int), np.zeros(3*M, dtype=complex)
+
+        for iT,(A,B,C) in enumerate(mesh.faces):
+            aT = 2*area[iT]
+            xA,yA = conn.project(mesh.vertices[A], iT)
+            xB,yB = conn.project(mesh.vertices[B], iT)
+            xC,yC = conn.project(mesh.vertices[C], iT)
+
+            rows[3*iT],   cols[3*iT],   vals[3*iT]   = iT, A, complex(yB-yC, xC-xB) / aT
+            rows[3*iT+1], cols[3*iT+1], vals[3*iT+1] = iT, B, complex(yC-yA, xA-xC) / aT
+            rows[3*iT+2], cols[3*iT+2], vals[3*iT+2] = iT, C, complex(yA-yB, xB-xA) / aT
+
+    else:
+        cols, rows, vals = np.zeros(6*M, dtype=int), np.zeros(6*M, dtype=int), np.zeros(6*M, dtype=float)
+        for iT,(A,B,C) in enumerate(mesh.faces):
+            aT = 2*area[iT]
+            xA,yA = conn.project(mesh.vertices[A], iT)
+            xB,yB = conn.project(mesh.vertices[B], iT)
+            xC,yC = conn.project(mesh.vertices[C], iT)
+
+            rows[6*iT],   cols[6*iT],   vals[6*iT]   = 2*iT,   A, (yB-yC) / aT
+            rows[6*iT+1], cols[6*iT+1], vals[6*iT+1] = 2*iT+1, A, (xC-xB) / aT
+            rows[6*iT+2], cols[6*iT+2], vals[6*iT+2] = 2*iT,   B, (yC-yA) / aT
+            rows[6*iT+3], cols[6*iT+3], vals[6*iT+3] = 2*iT+1, B, (xA-xC) / aT
+            rows[6*iT+4], cols[6*iT+4], vals[6*iT+4] = 2*iT,   C, (yA-yB) / aT
+            rows[6*iT+5], cols[6*iT+5], vals[6*iT+5] = 2*iT+1, C, (xB-xA) / aT
+        M *= 2
+    return sp.csc_matrix((vals, (rows,cols)), shape=(M,N))
