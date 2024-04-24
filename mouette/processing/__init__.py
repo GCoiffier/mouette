c155d7bdf8b3e3f6d987769155158374534d3e07
Guillaume <guillaume.coiffier.42@orange.fr>
2024-04-24 11:00:41 +0200

    refactor sampling functions

--- mouette/processing/__init__.py
+++ mouette/processing/__init__.py
@@ -7,13 +7,11 @@ from .misc import *
 from .combinatorics.subdivide import *
 from .combinatorics.cutting import SingularityCutter
 from .features import FeatureEdgeDetector
-from .space_partition import QuadTree
 from .expmap import DiscreteExponentialMap
 
 from .paths import shortest_path, shortest_path_to_border, shortest_path_to_vertex_set
 from .border import extract_border_cycle, extract_border_cycle_all, extract_curve_boundary, extract_surface_boundary
 
 from . import trees
-from . import sampling
 from .parametrization import distortion
 from .connection import SurfaceConnectionFaces, SurfaceConnectionVertices, FlatConnectionVertices, FlatConnectionFaces
